2a8ac62ef4b2f323e9ac9793fa0176b3f854f919
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2025-04-26 13:22:26 +0530

    feat: cousera nlp week 1

--- cousera_nlp/course1/week1_logistic_regression/utils.py
+++ cousera_nlp/course1/week1_logistic_regression/utils.py
@@ -1,6 +1,7 @@
 import re
 import string
 
+import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
@@ -38,19 +39,66 @@ def perform_stemming(tokens: list[str]) -> list[str]:
     return stemmed_text
 
 
-def process_tweet(text: str) -> list[str]:
-    cleaned_text = clean_text(text)
-    tokens = tokenize_text(cleaned_text)
-    cleaned_tokens = remove_stopwords_punctuations(tokens)
-    stemmed_text = perform_stemming(cleaned_tokens)
-    return stemmed_text
+def process_tweet(tweet: str) -> list[str]:
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words("english")
+    # remove stock market tickers like $GE
+    tweet = re.sub(r"\$\w*", "", tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r"^RT[\s]+", "", tweet)
+    # remove hyperlinks
+    tweet = re.sub(r"https?://[^\s\n\r]+", "", tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r"#", "", tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (
+            word not in stopwords_english  # remove stopwords
+            and word not in string.punctuation
+        ):  # remove punctuation
+            # tweets_clean.append(word)
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
 
 
-def build_freqs(tweets, ys):
-    assert len(tweets) == len(ys), "length of labels should match the length of tweets"
+def build_freqs(tweets: list[str], ys: np.ndarray) -> dict:
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet
+            (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its
+        frequency
+    """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
     freqs = {}
-    for tweet, y in zip(tweets, ys):
-        for token in process_tweet(tweet):
-            token_label_pair = (token, y)
-            freqs[token_label_pair] = freqs.get(token_label_pair, 0) + 1
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
     return freqs
